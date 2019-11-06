5e95c5978fa9f7ef7e47e73de65226b3c9a54071
danyelhenrique <danyelhenriquefidel@gmail.com>
2019-11-06 16:12:02 -0300

    install dependencies , finish basic config of server and make User Model with noSql mongoDb

---
+++ src/app/models/User.js
@@ -0,0 +1,24 @@
+const monogoose = require('mongoose')
+
+const UserSchema = new monogoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = monogoose.model('User', UserSchema)
---
+++ src/config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+  uri: `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0-l8thx.mongodb.net/test?retryWrites=true&w=majority`
+}
